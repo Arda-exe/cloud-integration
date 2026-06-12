5ee783adf0b74ec421abee2065ed24a331b5f21d
Arda <aydemirarda10@gmail.com>
2026-06-12 11:48:04 +0200

    app-skelet gebouwd met 3 tabs die je kan navigeren

---
+++ app/dashboard/webapp/controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("primepath.dashboard.controller.App", {
+
+        onTabSelect: function (oEvent) {
+            // navigatie via router komt in stap 2 (routing)
+        }
+
+    });
+});

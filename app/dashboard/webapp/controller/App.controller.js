49660b52b0c98ea3c1b143e3d681d505f2317a5e
Arda <aydemirarda10@gmail.com>
2026-06-14 22:55:09 +0200

    glaboal search functie toegevoegd

--- app/dashboard/webapp/controller/App.controller.js
+++ app/dashboard/webapp/controller/App.controller.js
@@ -1,6 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, Fragment, JSONModel, MessageToast) {
     "use strict";
 
     return Controller.extend("primepath.dashboard.controller.App", {
@@ -22,6 +25,145 @@ sap.ui.define([
         onTabSelect: function (oEvent) {
             this.getOwnerComponent().getRouter()
                 .navTo(oEvent.getParameter("key"));
+        },
+
+        // ---- Global search (feature 7) -------------------------------------
+
+        // People/Airports/Airlines zijn klein en sessie-statisch → één keer cachen
+        _ensureSearchData: function () {
+            if (this._pSearchData) {
+                return this._pSearchData;
+            }
+            var oComponent = this.getOwnerComponent();
+            var fnList = function (sModel, sPath) {
+                return oComponent.getModel(sModel).bindList(sPath).requestContexts(0, 500)
+                    .then(function (aContexts) {
+                        return aContexts.map(function (oContext) { return oContext.getObject(); });
+                    });
+            };
+            this._pSearchData = Promise.all([
+                fnList("people", "/People"),
+                fnList("airports", "/Airports"),
+                fnList("airlines", "/Airlines")
+            ]).then(function (aResults) {
+                return { people: aResults[0], airports: aResults[1], airlines: aResults[2] };
+            });
+            return this._pSearchData;
+        },
+
+        onGlobalSearch: function (oEvent) {
+            var that = this;
+            var sQuery = (oEvent.getParameter("query") || "").toLowerCase().trim();
+            if (!sQuery) {
+                return;
+            }
+            this._ensureSearchData().then(function (oData) {
+                var fnMatch = function (aFields) {
+                    return aFields.some(function (sField) {
+                        return (sField || "").toLowerCase().indexOf(sQuery) !== -1;
+                    });
+                };
+                var aEmp = oData.people.filter(function (p) {
+                    return fnMatch([p.FirstName, p.LastName, p.UserName]);
+                }).slice(0, 8).map(function (p) {
+                    return {
+                        type: "employee",
+                        title: (p.FirstName || "") + " " + (p.LastName || ""),
+                        desc: p.UserName,
+                        key: p.UserName,
+                        icon: "sap-icon://employee"
+                    };
+                });
+                var aApt = oData.airports.filter(function (a) {
+                    return fnMatch([a.Name, a.IataCode,
+                        a.Location && a.Location.City && a.Location.City.Name]);
+                }).slice(0, 8).map(function (a) {
+                    return {
+                        type: "airport",
+                        title: a.Name,
+                        desc: a.IataCode,
+                        key: a.IataCode,
+                        icon: "sap-icon://flight"
+                    };
+                });
+                var aAln = oData.airlines.filter(function (a) {
+                    return fnMatch([a.Name, a.AirlineCode]);
+                }).slice(0, 8).map(function (a) {
+                    return {
+                        type: "airline",
+                        title: a.Name,
+                        desc: a.AirlineCode,
+                        key: a.AirlineCode,
+                        icon: "sap-icon://globe"
+                    };
+                });
+
+                if (!aEmp.length && !aApt.length && !aAln.length) {
+                    MessageToast.show(that._bundle().getText("globalSearchNoResults"));
+                    return;
+                }
+
+                var oSearchModel = new JSONModel({
+                    query: sQuery,
+                    employees: aEmp, airports: aApt, airlines: aAln,
+                    empCount: aEmp.length, airportCount: aApt.length, airlineCount: aAln.length
+                });
+                that._openSearchDialog(oSearchModel);
+            });
+        },
+
+        _openSearchDialog: function (oSearchModel) {
+            var that = this;
+            var fnShow = function () {
+                that._oSearchDialog.setModel(oSearchModel, "search");
+                that._oSearchDialog.open();
+            };
+            if (this._oSearchDialog) {
+                fnShow();
+                return;
+            }
+            Fragment.load({
+                id: this.getView().getId() + "--searchResults",
+                name: "primepath.dashboard.view.SearchResults",
+                controller: this
+            }).then(function (oDialog) {
+                that._oSearchDialog = oDialog;
+                that.getView().addDependent(oDialog);
+                fnShow();
+            });
+        },
+
+        onSearchItemPress: function (oEvent) {
+            var oItem = oEvent.getSource().getBindingContext("search").getObject();
+            var oRouter = this.getOwnerComponent().getRouter();
+            if (this._oSearchDialog) {
+                this._oSearchDialog.close();
+            }
+            if (oItem.type === "employee") {
+                oRouter.navTo("employee", { userName: oItem.key });
+            } else if (oItem.type === "airport") {
+                oRouter.navTo("airportFocus", { iata: oItem.key });
+            } else {
+                // geen airline-detailpagina → informatief
+                MessageToast.show(this._bundle().getText("airlineInfo", [oItem.title, oItem.key]));
+            }
+        },
+
+        onCloseSearch: function () {
+            if (this._oSearchDialog) {
+                this._oSearchDialog.close();
+            }
+        },
+
+        onExit: function () {
+            if (this._oSearchDialog) {
+                this._oSearchDialog.destroy();
+                this._oSearchDialog = null;
+            }
+        },
+
+        _bundle: function () {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle();
         }
 
     });
